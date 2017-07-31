9f8621d0a309913bcf8557b4bb0681ba56e7a45c
kimobrian <brian.kimokoti@andela.com>
2017-07-31 12:26:11 -0400

    chore: Create application reducers

---
+++ app/reducers/currentItem.js
@@ -0,0 +1,21 @@
+import { itemActions as actionTypes } from '../actions/actionTypes'
+
+let initialState = {
+    itemHasErrored: false,
+    itemIsLoading: false,
+    data: []
+}
+
+export default function items(state = {} , action) {
+    state = initialState;
+    switch (action.type) {
+        case actionTypes.ITEM_HAS_ERRORED:
+            return Object.assign({}, state, { itemHasErrored: true, error: action.error });
+        case actionTypes.ITEM_IS_LOADING:
+            return Object.assign({}, state, { itemIsLoading: action.isLoading });
+        case actionTypes.ITEM_FETCH_DATA_SUCCESS:
+            return Object.assign({}, state, { itemIsLoading: action.isLoading, data:action.items })
+        default:
+            return state;
+    }
+}
---
+++ app/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux'
+import items from './items';
+import currentItem from './currentItem';
+
+export default combineReducers({
+    items,
+    currentItem
+});
