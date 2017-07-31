98321a280fcb5ae7f85b6c2c531bdb0f87b8a3b0
kimobrian <brian.kimokoti@andela.com>
2017-07-31 12:23:59 -0400

    chore:Create router and bootstrap with redux and MUI theme provider

---
+++ app/config/routes.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from '../store/configureStore';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import ItemList from '../components/ItemList';
+import TestComponent from '../components/ItemTest';
+import Details from '../components/Details';
+import Links from '../components/Links';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+var store = configureStore();
+
+const routes = (
+    <Provider store={store}>
+        <MuiThemeProvider>
+            <BrowserRouter>
+              <div>
+                  <Links />
+                  <Switch>
+                      <Route exact path="/" component={ItemList} />
+                      <Route path="/item/:id" component = {Details} />
+                      <Route path="/item" component={TestComponent} />
+                  </Switch>
+              </div>
+            </BrowserRouter>
+        </MuiThemeProvider>
+    </Provider>
+);
+
+export default routes;
